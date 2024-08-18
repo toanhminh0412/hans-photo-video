cfc2a66c6c2abe424043f540e7650686e97f9f4a
Archie To <toanhminh0412@gmail.com>
2024-08-18 11:16:19 -0700

    Publish Services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,30 +1,52 @@
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import Link from "next/link"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import { faPassport, faPortrait, faFilm, faImage, faPaintBrush, faCartShopping, faAnglesRight } from "@fortawesome/free-solid-svg-icons"
 
 const features = [
-  {
-    name: 'Push to deploy',
-    description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security',
-    description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
-  },
+    {
+        name: 'ID/Passport Photos',
+        description:
+        'We produce passport photos according to a variety of country specific requirements, including Canada, USA, Australia, Brazil, China, Europe, India, Iran, Japan, Korea, New Zealand, Philippine, Russia, Taiwan, Vietnam, and more!',
+        icon: faPassport,
+        href: '/id-passport-photos',
+    },
+    {
+        name: 'Headshots & Portraits',
+        description:
+        "Business Headshots, Actor's Headshots, Linkedin Headshots, Profile Pictures, Family Portraits, Children's Portraits, Graduation Portraits.",
+        icon: faPortrait,
+        href: '/headshots-portraits',
+    },
+    {
+        name: 'Film/Video/Audio Transfer',
+        description:
+        'Bring your videotapes and film reels to be transferred from their original analog formats into digital video.',
+        icon: faFilm,
+        href: '/filmvideoaudio-transfer',
+    },
+    {
+        name: 'Photo Restoration/Edit',
+        description:
+        'Bring in your treasurable old memories, and Hanns Photo Video will bring them back to life with our high tech photo retouching.',
+        icon: faImage,
+        href: '/photo-restorationedit',
+    },
+    {
+        name: 'Ink Refill',
+        description:
+        'We strongly recommend replacing or refilling empty ink cartridges as soon as they run out and refrain from printing on an empty cartridge, as this will damage your printhead.',
+        icon: faPaintBrush,
+        href: '/ink-refill',
+    },
+    {
+        name: 'SHOP INK & TONER',
+        description:
+        'Save your money by buying compatible toner/ink cartridges at Hanns Photo Video.',
+        icon: faCartShopping,
+        href: '/shop-ink-toner',
+    },
 ]
 
 export default function Services() {
@@ -32,29 +54,30 @@ export default function Services() {
         <section id="services" className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:text-center">
-                <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    Everything you need to deploy your app
-                </p>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                    Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-                    pulvinar et feugiat blandit at. In mi viverra elit nunc.
-                </p>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        Our services
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        We offer a wide range of services to help you with anything you need
+                    </p>
                 </div>
                 <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-                    {features.map((feature) => (
-                    <div key={feature.name} className="relative pl-16">
-                        <dt className="text-base font-semibold leading-7 text-gray-900">
-                        <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                            <feature.icon aria-hidden="true" className="h-6 w-6 text-white" />
-                        </div>
-                        {feature.name}
-                        </dt>
-                        <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-                    </div>
-                    ))}
-                </dl>
+                    <dl className="grid max-w-xl grid-cols-1 gap-x-12 gap-y-12 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                        {features.map((feature) => (
+                        <Link href={feature.href} key={feature.name} className="p-6 cursor-pointer duration-100 hover:shadow-lg hover:border hover:border-gray-200 rounded-md">
+                            <div className="relative pl-16">
+                                <dt className="text-base font-semibold leading-7 text-gray-900">
+                                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-yellow-300">
+                                        <FontAwesomeIcon icon={feature.icon} aria-hidden="true" className="h-6 w-6" />
+                                    </div>
+                                    {feature.name}
+                                </dt>
+                                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                                <dd className="mt-2 text-base leading-7 text-yellow-700 flex gap-2 items-center">Learn more <FontAwesomeIcon icon={faAnglesRight} className="w-4 h-4"/></dd>
+                            </div>
+                        </Link>
+                        ))}
+                    </dl>
                 </div>
             </div>
         </section>
